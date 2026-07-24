a3d8e25c61098d2969fcb043e66b64156bb87d41
Islam Moeen <islammoeen786@gmail.com>
2026-07-24 14:34:59 +0500

    Update API URL to Belmo production backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -6,7 +6,7 @@ class ApiService {
   late final Dio _dio;
 
   static const String _devUrl = 'http://localhost:3000/api';
-  static const String _prodUrl = 'https://2bb55ced11f1e0.lhr.life/api';
+  static const String _prodUrl = 'https://backend-3f80.onbelmo.uk/api';
 
   static String get defaultBaseUrl => kReleaseMode ? _prodUrl : _devUrl;
 
